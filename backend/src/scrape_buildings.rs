a9872786cd8c9cf20228ff61b13cbfb9c0fe12b3
Dustin Carlino <dabreegster@gmail.com>
2025-07-12 16:03:37 +0200

    Pick up buildings from relations

--- backend/src/scrape_buildings.rs
+++ backend/src/scrape_buildings.rs
@@ -7,7 +7,10 @@ use utils::{osm2graph::OsmReader, Tags};
 // TODO Ignores holes
 #[derive(Default)]
 pub struct OsmBuildings {
+    // TODO Maybe OsmID instead, to cover relations
     pub polygons: Vec<(WayID, Polygon)>,
+
+    possible_building_parts: HashMap<WayID, Polygon>,
 }
 
 impl OsmReader for OsmBuildings {
@@ -20,6 +23,17 @@ impl OsmReader for OsmBuildings {
         node_mapping: &HashMap<NodeID, Coord>,
         tags: &Tags,
     ) {
+        if tags.0.is_empty() && node_ids[0] == *node_ids.last().unwrap() {
+            self.possible_building_parts.insert(
+                id,
+                Polygon::new(
+                    LineString::new(node_ids.into_iter().map(|n| node_mapping[n]).collect()),
+                    Vec::new(),
+                ),
+            );
+            return;
+        }
+
         if !tags.has("building") {
             return;
         }
@@ -28,5 +42,20 @@ impl OsmReader for OsmBuildings {
         self.polygons.push((id, Polygon::new(exterior, Vec::new())));
     }
 
-    fn relation(&mut self, _: RelationID, _: &Vec<(String, OsmID)>, _: &Tags) {}
+    fn relation(&mut self, _: RelationID, members: &Vec<(String, OsmID)>, tags: &Tags) {
+        if !tags.has("building") {
+            return;
+        }
+        for (role, id) in members {
+            // TODO if-let
+            if role != "outer" {
+                continue;
+            }
+            if let OsmID::Way(way) = id {
+                if let Some(polygon) = self.possible_building_parts.remove(&way) {
+                    self.polygons.push((*way, polygon));
+                }
+            }
+        }
+    }
 }
