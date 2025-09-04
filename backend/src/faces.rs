3fe6b8ab1aac96484bce0de7ee68d6a4f8597f1a
Dustin Carlino <dabreegster@gmail.com>
2025-09-04 12:00:34 +0100

    Upstream split_polgon to utils

--- backend/src/faces.rs
+++ backend/src/faces.rs
@@ -2,13 +2,12 @@ use std::collections::{BTreeMap, BTreeSet};
 
 use geo::buffer::{BufferStyle, LineJoin};
 use geo::{
-    BooleanOps, BoundingRect, Buffer, Centroid, Contains, Coord, Distance, Euclidean,
-    InterpolatableLine, LineString, MultiLineString, Point, Polygon, Rect,
+    BooleanOps, BoundingRect, Buffer, Centroid, Contains, Distance, Euclidean, InterpolatableLine,
+    LineString, MultiLineString, Point, Polygon, Rect,
 };
 use geojson::Feature;
-use i_overlay::core::fill_rule::FillRule;
-use i_overlay::float::slice::FloatSlice;
 use rstar::{primitives::GeomWithData, RTree, AABB};
+use utils::split_polygon;
 
 use crate::geo_helpers::SliceNearEndpoints;
 use crate::{
@@ -132,45 +131,6 @@ pub fn make_faces(graph: &Graph, areas: &Areas) -> BTreeMap<FaceID, Face> {
     }
     info!("Done");
     faces
-}
-
-// TODO Revisit some of this; conversions are now in geo
-fn split_polygon<'a>(
-    polygon: &Polygon,
-    lines: impl Iterator<Item = &'a LineString>,
-) -> Vec<Polygon> {
-    let mut shape = to_i_overlay_contour(polygon.exterior());
-
-    // geo Polygon's are explicitly closed LineStrings, but i_overlay Polygon's are not.
-    shape.pop();
-
-    let splitters: Vec<_> = lines.map(to_i_overlay_contour).collect();
-    let shapes = shape.slice_by(&splitters, FillRule::NonZero);
-
-    shapes
-        .into_iter()
-        .map(|rings| {
-            let mut linestrings: Vec<LineString> =
-                rings.into_iter().map(to_geo_linestring).collect();
-            if linestrings.is_empty() {
-                panic!("a split shape is empty");
-            }
-            let exterior = linestrings.remove(0);
-            Polygon::new(exterior, linestrings)
-        })
-        .collect()
-}
-
-fn to_geo_linestring(pts: Vec<[f64; 2]>) -> LineString {
-    LineString(
-        pts.into_iter()
-            .map(|pt| Coord { x: pt[0], y: pt[1] })
-            .collect(),
-    )
-}
-
-fn to_i_overlay_contour(line_string: &LineString) -> Vec<[f64; 2]> {
-    line_string.coords().map(|c| [c.x, c.y]).collect()
 }
 
 fn linestring_along_polygon(ls: &LineString, polygon: &Polygon) -> bool {
